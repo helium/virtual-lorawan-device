43f8094a6de1e95b5c41918422edd3db58482ffb
lthiery <me@louisthiery.com>
2020-06-09 20:00:25 -0700

    Clean up and prepare release

---
+++ CHANGELOG.md
@@ -0,0 +1,27 @@
+<!--
+M.m.p (YYYY-MM-DD)
+==================
+Add a summary of this release.
+
+**BREAKING CHANGES**:
+
+* Some change which breaks API or ABI compatiblity with.
+
+
+Feature enhancements:
+
+* [Link to github PR]():
+  A new feature.
+
+Bug fixes:
+
+* [Link to github PR]():
+  A bugfix.
+-->
+0.1.0 (2020-06-02)
+==================
+Initial release:
+* Miner connection is optionally configurable via command line
+* device credentials and uplink rate defined via lorawan-device.json
+* tolerates ACK packets only
+* prints timeliness of Downlinks relative to RxWindow1
---
+++ src/config.rs
@@ -0,0 +1,59 @@
+use serde_derive::{Deserialize, Serialize};
+use std::fs;
+use std::path::Path;
+
+#[derive(Clone, Deserialize, Serialize, Debug)]
+pub struct Credentials {
+    app_eui: String,
+    app_key: String,
+    dev_eui: String,
+}
+
+impl Credentials {
+    pub fn appeui_cloned_into_buf(&self) -> Result<[u8; 8], Box<dyn std::error::Error>> {
+        let vec = hex::decode(&self.app_eui)?;
+        Ok([
+            vec[7], vec[6], vec[5], vec[4], vec[3], vec[2], vec[1], vec[0],
+        ])
+    }
+    pub fn deveui_cloned_into_buf(&self) -> Result<[u8; 8], Box<dyn std::error::Error>> {
+        let vec = hex::decode(&self.dev_eui)?;
+        Ok([
+            vec[7], vec[6], vec[5], vec[4], vec[3], vec[2], vec[1], vec[0],
+        ])
+    }
+    pub fn appkey_cloned_into_buf(&self) -> Result<[u8; 16], Box<dyn std::error::Error>> {
+        let vec = hex::decode(&self.app_key)?;
+        Ok([
+            vec[0], vec[1], vec[2], vec[3], vec[4], vec[5], vec[6], vec[7], vec[8], vec[9],
+            vec[10], vec[11], vec[12], vec[13], vec[14], vec[15],
+        ])
+    }
+}
+
+#[derive(Clone, Deserialize, Serialize, Debug)]
+pub struct Device {
+    transmit_delay: usize,
+    credentials: Credentials,
+}
+
+impl Device {
+    pub fn credentials(&self) -> &Credentials {
+        &self.credentials
+    }
+
+    pub fn transmit_delay(&self) -> u64 {
+        self.transmit_delay as u64
+    }
+}
+
+pub fn load_devices(path: &str) -> Result<Option<Vec<Device>>, Box<dyn std::error::Error>> {
+    if !Path::new(path).exists() {
+        println!("No lorawan-devices.json found");
+        return Ok(None);
+    }
+
+    let contents = fs::read_to_string(path)?;
+    let devices: Vec<Device> = serde_json::from_str(&contents)?;
+    Ok(Some(devices))
+}
