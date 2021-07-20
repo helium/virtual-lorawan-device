044f7f7ff609444d52eb1ef0abdaf96e54babfe0
Louis Thiery <louis@helium.com>
2021-07-20 11:20:28 -0700

    add settings via toml file (#13)

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,44 @@
-use std::net::SocketAddr;
-use std::str::FromStr;
-use std::time::Instant;
-
+use std::{net::SocketAddr, path::PathBuf, str::FromStr, time::Instant};
 use structopt::StructOpt;
 
 mod error;
+mod settings;
 mod virtual_device;
 
-pub use error::Result;
-
-pub struct Credentials {
-    deveui: [u8; 8],
-    appeui: [u8; 8],
-    appkey: [u8; 16],
-}
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    let mac_address = [0, 0, 0, 0, 4, 3, 2, 1];
-    let cli = Opt::from_args();
-    let host = SocketAddr::from_str(cli.host.as_str())?;
-    let instant = Instant::now();
-
-    let lorawan_app = virtual_device::VirtualDevice::new(
-        instant,
-        host,
-        mac_address,
-        Credentials {
-            deveui: [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8],
-            appeui: [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8],
-            appkey: [
-                0xFB, 0x4B, 0x19, 0xE9, 0xF8, 0xD4, 0xB1, 0x50, 0x35, 0x76, 0xCE, 0x9B, 0xD8, 0x79,
-                0x9C, 0xD3,
-            ],
-        },
-    )
-    .await;
-
-    lorawan_app.run().await
-}
+pub use error::{Error, Result};
+pub use settings::Credentials;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "virtual-lorawan-device", about = "LoRaWAN test device utility")]
 pub struct Opt {
-    #[structopt(short, long, default_value = "127.0.0.1:1680")]
-    pub host: String,
+    #[structopt(short, long, default_value = "./settings")]
+    pub settings: PathBuf,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = Opt::from_args();
+    let instant = Instant::now();
+    let settings = settings::Settings::new(&cli.settings)?;
+    let host = SocketAddr::from_str(settings.host.as_str())?;
+
+    for (label, device) in settings.devices {
+        let lorawan_app = virtual_device::VirtualDevice::new(
+            instant,
+            host,
+            device.mac_cloned_into_buf()?,
+            device.credentials,
+        )
+        .await?;
+
+        tokio::spawn(async move {
+            if let Err(e) = lorawan_app.run().await {
+                println!("{} device threw error: {:?}", label, e)
+            }
+        });
+    }
+
+    tokio::signal::ctrl_c().await?;
+    println!("User exit via ctrl C");
+    Ok(())
 }
---
+++ src/settings.rs
@@ -0,0 +1,72 @@
+use super::Result;
+use config::{Config, File};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, path::Path};
+
+#[derive(Deserialize, Debug)]
+pub struct Settings {
+    pub host: String,
+    pub devices: HashMap<String, Device>,
+}
+
+impl Settings {
+    /// Load Settings from a given path. Settings are loaded from a default.toml
+    /// file in the given path, followed by merging in an optional settings.toml
+    /// in the same folder.
+    pub fn new(path: &Path) -> Result<Settings> {
+        let mut c = Config::new();
+        let default_file = path.join("default.toml");
+        // Load default config and merge in overrides
+        c.merge(File::with_name(default_file.to_str().expect("file name")))?;
+        let settings_file = path.join("settings.toml");
+        if settings_file.exists() {
+            c.merge(File::with_name(settings_file.to_str().expect("file name")))?;
+        }
+        c.try_into().map_err(|e| e.into())
+    }
+}
+
+#[derive(Clone, Deserialize, Serialize, Debug)]
+pub struct Device {
+    mac: String,
+    pub credentials: Credentials,
+}
+
+impl Device {
+    pub fn mac_cloned_into_buf(&self) -> Result<[u8; 8]> {
+        let vec = hex::decode(&self.mac)?;
+        Ok([
+            vec[7], vec[6], vec[5], vec[4], vec[3], vec[2], vec[1], vec[0],
+        ])
+    }
+}
+
+#[derive(Clone, Deserialize, Serialize, Debug)]
+pub struct Credentials {
+    pub app_eui: String,
+    pub app_key: String,
+    pub dev_eui: String,
+}
+
+impl Credentials {
+    pub fn appeui_cloned_into_buf(&self) -> Result<[u8; 8]> {
+        let vec = hex::decode(&self.app_eui)?;
+        Ok([
+            vec[7], vec[6], vec[5], vec[4], vec[3], vec[2], vec[1], vec[0],
+        ])
+    }
+
+    pub fn deveui_cloned_into_buf(&self) -> Result<[u8; 8]> {
+        let vec = hex::decode(&self.dev_eui)?;
+        Ok([
+            vec[7], vec[6], vec[5], vec[4], vec[3], vec[2], vec[1], vec[0],
+        ])
+    }
+    pub fn appkey_cloned_into_buf(&self) -> Result<[u8; 16]> {
+        let vec = hex::decode(&self.app_key)?;
+        Ok([
+            vec[0], vec[1], vec[2], vec[3], vec[4], vec[5], vec[6], vec[7], vec[8], vec[9],
+            vec[10], vec[11], vec[12], vec[13], vec[14], vec[15],
+        ])
+    }
+}
