4fda3093e7a623c5b6fa0e77bd072cf94b60a72d
Kent Williams <k3nt00@gmail.com>
2021-07-30 15:07:53 -0700

    add prom serv to startup

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,39 @@ mod virtual_device;
 pub use error::{Error, Result};
 pub use settings::Credentials;
 
+use hyper::{
+    header::CONTENT_TYPE,
+    service::{make_service_fn, service_fn},
+    Body, Request, Response, Server,
+};
+use prometheus::{Encoder, TextEncoder};
+
 #[derive(Debug, StructOpt)]
 #[structopt(name = "virtual-lorawan-device", about = "LoRaWAN test device utility")]
 pub struct Opt {
     #[structopt(short, long, default_value = "./settings")]
     pub settings: PathBuf,
+}
+
+async fn serve_req(_req: Request<Body>) -> Result<Response<Body>> {
+    let encoder = TextEncoder::new();
+
+    let metric_families = prometheus::gather();
+    let mut buffer = vec![];
+    let mut buffer_print = vec![];
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+    encoder.encode(&metric_families, &mut buffer_print).unwrap();
+
+    // Output to the standard output.
+    info!("{}", String::from_utf8(buffer_print).unwrap());
+
+    let response = Response::builder()
+        .status(200)
+        .header(CONTENT_TYPE, encoder.format_type())
+        .body(Body::from(buffer))
+        .unwrap();
+
+    Ok(response)
 }
 
 #[tokio::main]
@@ -42,6 +70,19 @@ async fn main() -> Result<()> {
             }
         });
     }
+
+    let addr = ([127, 0, 0, 1], 9898).into();
+    println!("Listening on http://{}", addr);
+
+    let serve_future = Server::bind(&addr).serve(make_service_fn(|_| async {
+        Ok::<_, hyper::Error>(service_fn(serve_req))
+    }));
+
+    tokio::spawn(async move {
+        if let Err(e) = serve_future.await {
+            error!("prometheus serv threw error: {:?}", e)
+        }
+    });
 
     tokio::signal::ctrl_c().await?;
     info!("User exit via ctrl C");
